12572f8e696bc3d98363fa6e88118bb493689e33
Nisha Saraswat <nisha.saraswat000@gmail.com>
2024-01-22 15:39:24 +0530

    Implementation of Apache Kafka

---
+++ deliverybody/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,11 @@
+package com.deliveryboy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConstants {
+
+    public static final String LOCATION_TOPIC_NAME = "location-update-topic";
+
+
+}
